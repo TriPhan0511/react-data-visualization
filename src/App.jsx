1a042f77c2f640eb9095e62cac70b03c6a7da5b3
Tri Phan <tripp@ames.edu.vn>
2023-02-10 15:41:35 +0700

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import MuiTable from './components/material-ui-tables/MuiTable'
 import BarChartBasicColumn from './components/highcharts/BarChartBasicColumn'
 import DualAxesLineAndColumn from './components/highcharts/DualAxesLineAndColumn'
 import PieChart3D from './components/highcharts/PieChart3D'
+import StatBoxMenardDisplay from './components/statistic-box/StatBoxMenardDisplay'
 
 function App() {
-	return (
-		<div style={{ display: 'flex', justifyContent: 'center' }}>
-			<MuiDataGrid title='Báo cáo khóa học' />
-			{/* <MuiTable title='Khóa học nhiều người tham gia nhất' /> */}
-			{/* <PieChart3D title='Biểu đồ khóa học' /> */}
-			{/* <DualAxesLineAndColumn title='Biểu đồ lưu lượng người dùng' /> */}
-			{/* <BarChartBasicColumn title='Đơn vị có lượt tham gia học nhiều nhất' /> */}
-		</div>
-	)
+	return <StatBoxMenardDisplay />
+	// return (
+	// 	<div style={{ display: 'flex', justifyContent: 'center' }}>
+	// 		<MuiDataGrid title='Báo cáo khóa học' />
+	// 		{/* <MuiTable title='Khóa học nhiều người tham gia nhất' /> */}
+	// 		{/* <PieChart3D title='Biểu đồ khóa học' /> */}
+	// 		{/* <DualAxesLineAndColumn title='Biểu đồ lưu lượng người dùng' /> */}
+	// 		{/* <BarChartBasicColumn title='Đơn vị có lượt tham gia học nhiều nhất' /> */}
+	// 	</div>
+	// )
 }
 
 export default App
